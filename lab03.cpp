da195e7df55fe9846266d1cbb9cd88cc08349a0e
[Bini Chand] <[079bct0b33.bini@pcampus.edu.np]>
2024-12-11 18:14:40 +0545

    bisection method

---
+++ lab03.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float compFunc(float x)
+{
+    return (x*x*x -4*x - 9);
+}
+float compMean (float x1, float x2)
+{
+    return (x1 + x2) / 2;   
+}
+void bisection(float x1, float x2)
+{
+    float c;
+    int count=0;
+    do 
+    {
+        if (compFunc(x1) * compFunc(x2) > 0)
+        {
+            cout << "Root is not bracketed" << endl;
+            return;
+        }
+        c = compMean(x1, x2);
+        if (compFunc(c) == 0)
+        {
+            cout << "Root is " << c << endl;
+            return;
+        }
+        if (compFunc(c) * compFunc(x1) < 0)
+        {
+            x2 = c;
+        }
+        else
+        {
+            x1 = c;
+        }
+        count++;
+
+    }while (abs(compFunc(c)) > 0.001 && count < 100);
+    if (count == 100)
+    {
+        cout << "Root is not found" << endl;
+    }
+    cout << "Number of iterations: " << count << endl;
+    cout << "Root is " << c << endl;
+
+}
+int main()
+{
+    float x1, x2;
+    cout<<"enter the lower limit and upper limit of the bracket: "<<endl;
+    cin>>x1>>x2;
+    bisection(x1, x2);
+    return 0;
+}
